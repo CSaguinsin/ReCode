7a09d609fe397209c9c1f691a6e6584a40acce0f
Carl Saginsin <saguinsincarl8@gmail.com>
2024-06-24 19:34:22 +0700

    Finalized Manifest

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,14 @@
-import { defineConfig } from 'vite'
-import laravel from 'laravel-vite-plugin'
-buildDirectory: '../../public/build';
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    build: {
+        outDir: '../../public/build',
+    },
     plugins: [
         laravel({
             input: ['resources/sass/app.scss', 'resources/js/app.js'],
             refresh: true,
         }),
     ],
-})
+});
