85393a587d82c51853f4c0b02ee0abd670939256
Carl Saginsin <saguinsincarl8@gmail.com>
2024-06-24 23:45:06 +0700

    "Updated input field class in new-documentation.blade.php and reformatted Route definitions in web.php"

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ export default defineConfig({
     build: {
         outDir: '../../public/build',
     },
+
     plugins: [
         laravel({
             input: ['resources/sass/app.scss', 'resources/js/app.js'],
