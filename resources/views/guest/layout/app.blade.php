28b1273992a556d364909ddd02cb739ac0ac8a44
Carl Saginsin <saguinsincarl8@gmail.com>
2024-06-24 19:15:06 +0700

    Fixing manifest.json

--- resources/views/guest/layout/app.blade.php
+++ resources/views/guest/layout/app.blade.php
@@ -7,8 +7,8 @@
     <title>ReCode | Built for Developers - @yield('title')</title>
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
     <link href="public/build/manifest.json" rel="manifest">
-    <link href="public/build/assets/app-CCLi5Phe.css" rel="stylesheet">
-    <link href="public/build/assets/app-qVBN5XO4.js" rel="stylesheet">
+    <link href="public/build/assets/app.css" rel="stylesheet">
+    <link href="public/build/assets/app.js" rel="stylesheet">
 
 
 
