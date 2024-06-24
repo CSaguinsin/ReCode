2265cff3b8276c92c3904ac7c330ed848dd6745f
Carl Saginsin <saguinsincarl8@gmail.com>
2024-06-24 16:50:43 +0700

    something

--- resources/views/guest/about/hero.blade.php
+++ resources/views/guest/about/hero.blade.php
@@ -4,7 +4,7 @@
             <img src="{{ asset('assets/elements/aboutMe.png') }}" class="lg:w-[230px] lg:h-[266px]" />
         </div>
         <div>
-            <h1 class="text-white font-sans  font-bold text-[24px]">Hi👋, I’m Carl Saginsin</h1>
+            <h1 class="text-white font-sans  font-bold text-[24px]">Hi👋I’am Carl Saginsin</h1>
             <h1 class="text-white font-sans  font-bold text-[48px] w-[655px]">I love Coding, UI design and building software’s.</h1>
         </div>
     </div>
